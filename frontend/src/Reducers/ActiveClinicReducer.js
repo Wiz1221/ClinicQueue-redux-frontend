f2282816d7d9dd868cae2d1f45205961d1af7275
Wiz1221 <geokyan89@gmail.com>
2017-07-27 10:49:15 +0800

    Update ActiveClinicReducer.js

--- frontend/src/Reducers/ActiveClinicReducer.js
+++ frontend/src/Reducers/ActiveClinicReducer.js
@@ -4,12 +4,9 @@ const ActiveClinic = (state = {}, action) => {
     case 'ACTIVE_CLINIC':
       return action.clinic || {}
       break;
-    case 'STORE_NEW_QUEUE_IN_ACTIVE_CLINIC':
-      let queue = state.queue
-      queue.push(action.queue);
-      return Object.assign({}, state, {
-        queue: queue
-      })
+    case 'REMOVE_ACTIVE_CLINIC':
+      return {}
+      break;
     default:
       return state
   }
